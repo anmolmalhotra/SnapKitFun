92ba361489f48f5b464cd7523e473de02b9d48ff
Anmol Malhotra <anmol.github@gmail.com>
2017-07-31 17:47:18 +0530

    Initial Commit

---
+++ SnapKitFun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnapKitFun
+//
+//  Created by Anmol Malhotra on 31/07/17.
+//  Copyright © 2017 Anmol Malhotra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
